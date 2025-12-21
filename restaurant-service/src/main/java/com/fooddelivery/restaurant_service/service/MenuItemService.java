334e84db7bd6962e84e7e8ae4c4c7d7f4fe0d2ac
Prince Kumar <princekumaraman7654@gmail.com>
2025-12-21 18:28:46 +0530

    redis caching enabled for restaurant-service

--- restaurant-service/src/main/java/com/fooddelivery/restaurant_service/service/MenuItemService.java
+++ restaurant-service/src/main/java/com/fooddelivery/restaurant_service/service/MenuItemService.java
@@ -5,6 +5,8 @@ import com.fooddelivery.restaurant_service.Repository.MenuItemRepository;
 import com.fooddelivery.restaurant_service.Repository.RestaurantRepository;
 import com.fooddelivery.restaurant_service.restaurant.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +19,7 @@ public class MenuItemService {
     private final MenuItemRepository menuItemRepository;
     private final RestaurantRepository restaurantRepository;
 
+    @CacheEvict(value = "menus", key = "#restaurantId")
     public MenuItem createMenuItem(UUID restaurantId, MenuItemRequest request) {
         Restaurant restaurant = restaurantRepository.findById(restaurantId)
                 .orElseThrow(() -> new RuntimeException("Restaurant not found"));
@@ -33,6 +36,7 @@ public class MenuItemService {
         return menuItemRepository.save(item);
     }
 
+    @Cacheable(value = "menus", key = "#restaurantId")
     public List<MenuItem> getMenuForRestaurant(UUID restaurantId) {
         return menuItemRepository.findByRestaurant_Id(restaurantId);
     }
