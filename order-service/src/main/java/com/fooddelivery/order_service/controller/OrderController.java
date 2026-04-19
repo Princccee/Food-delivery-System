cf0c20b215c7666b53d107f09b302cfa67fd9025
Prince Kumar <princekumaraman7654@gmail.com>
2026-04-19 15:24:45 +0530

    remove /api route in order-service

--- order-service/src/main/java/com/fooddelivery/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/fooddelivery/order_service/controller/OrderController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
